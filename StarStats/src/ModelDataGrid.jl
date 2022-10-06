29f8b8373dd8a7f2bc01645ee316273958e37610
Alina <al.istrate@gmail.com>
2022-10-06 22:43:59 +0200

    Added partial comments to the module

--- StarStats/src/ModelDataGrid.jl
+++ StarStats/src/ModelDataGrid.jl
@@ -2,6 +2,18 @@ using CSV, DataFrames, CodecZlib
 
 export ModelDataGrid, load_grid
 
+"""
+    ModelDataGrid
+
+Struct containing the definition of a grid including the input variables defining it and their values 
+
+# Fields: 
+-   dfs: Multidimensional array of DataFrames containing individual evolutionary track information from a grid of models.
+        This is populated with the load_grid function.    
+- inputs: Vector of String vectors containing the parth of the path from a track. (TODO give an example)
+- input_names: Vector of Symbol containing the names of each parameter of the grid.  
+- input_values: Result of parsing the inputs into floats.   
+"""
 mutable struct ModelDataGrid
     dfs::Array{DataFrame}
     inputs::Vector{Vector{String}}
