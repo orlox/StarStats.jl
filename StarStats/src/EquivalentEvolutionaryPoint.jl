4ba4c19f044b56d6ff90fbd750301832455817c2
Pablo Marchant <pamarca@gmail.com>
2022-12-01 22:10:02 +0100

    optimized interpolation

--- StarStats/src/EquivalentEvolutionaryPoint.jl
+++ StarStats/src/EquivalentEvolutionaryPoint.jl
@@ -81,24 +81,47 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
     return EEPs
 end
 
-function get_secondary_EEP(x, track, interpolated_quantity)
+function get_secondary_EEP(x::T, track, interpolated_quantity)::T where{T}
     get_secondary_EEP_internal(x, track.x, getproperty(track, interpolated_quantity))
 end
-function get_secondary_EEP_internal(x, trackx, interp_values)
+function get_secondary_EEP_internal(x::T, trackx, interp_values)::T where{T}
     #dirty check to not overflow at edge
     lower_i = min(searchsorted(trackx,x).stop, length(trackx)-1)
     return interp_values[lower_i] + (x-trackx[lower_i])/(trackx[lower_i+1]-trackx[lower_i])*(interp_values[lower_i+1]-interp_values[lower_i]) # interpolate in x
 end
 
-function interpolate_grid_quantity(grid, grid_parameters, interpolated_quantity, x)
-    lower_i = zeros(Int, length(grid.input_names))
-    grid_values = zeros(length(grid.input_names),2)
+function interpolate_grid_quantity(grid, grid_parameters, interpolated_quantity, x::T) where{T}
+    return interpolate_grid_quantity_internal(grid.dfs, grid.input_values, grid_parameters, interpolated_quantity, x)
+end
+
+function df_index_element(i, lower_i, cartesian_index)
+    return cartesian_index[i]+lower_i[i]-1
+end
+function interpolator_index1_element(j,i)
+    if j<= i
+        return 1
+    end
+    return 2
+end
+function interpolator_index2_element(j,i,index1)
+    if j< i
+        return 1
+    elseif j==i
+        return 2
+    end
+    return index1[j]
+end
+
+function interpolate_grid_quantity_internal(dfs, input_values, grid_parameters, interpolated_quantity, x::T) where{T}
+    dimensions = ndims(dfs)
+    lower_i = zeros(Int, dimensions)
+    grid_values = zeros(dimensions,2)
     # get values of parameters at edges
-    for j in 1:length(grid.input_names)
-        for i in 1:(length(grid.input_values[j])-1)
-            if grid.input_values[j][i] <= grid_parameters[j] && grid.input_values[j][i+1] >= grid_parameters[j]
-                grid_values[j,1] = grid.input_values[j][i]
-                grid_values[j,2] = grid.input_values[j][i+1]
+    for j in 1:dimensions
+        for i in 1:(length(input_values[j])-1)
+            if input_values[j][i] <= grid_parameters[j] && input_values[j][i+1] >= grid_parameters[j]
+                grid_values[j,1] = input_values[j][i]
+                grid_values[j,2] = input_values[j][i+1]
                 lower_i[j] = i
                 break
             end
@@ -111,27 +134,28 @@ function interpolate_grid_quantity(grid, grid_parameters, interpolated_quantity,
     end
 
     # get value of quantity to interpolate at vertices at a given x
-    yvalues = zeros(typeof(x),[2 for i in 1:length(grid.input_names)]...)
-    df_index = zeros(Int, length(grid.input_names))
-    for index in collect(Base.product([1:2 for i in 1:length(grid.input_names)]...))
-        df_index .= lower_i.+index.-1
-        if !isassigned(grid.dfs,df_index...)
+    yvalues::Array{T,dimensions} = zeros(T,ntuple(_->2, dimensions)...)
+    for index in CartesianIndices(yvalues)
+        df_index = ntuple(i->df_index_element(i,lower_i,index),dimensions)
+        try
+            df = dfs[CartesianIndex(df_index)]
+            yvalues[index] = get_secondary_EEP(x, df, interpolated_quantity)
+        catch
             return NaN
         end
-        yvalues[index...] = get_secondary_EEP(x,grid.dfs[df_index...], interpolated_quantity)
     end
 
-    for i in 1:length(grid.input_names)
-        for index in Base.product([1:2 for i in 1:(length(grid.input_names)-i)]...)
-            yval1 = yvalues[[1 for j in 1:(i-1)]...,1,index...]
-            yval2 = yvalues[[1 for j in 1:(i-1)]...,2,index...]
-            xval1 = grid_values[i,1]
-            xval2 = grid_values[i,2]
-            xtarget = grid_parameters[i]
-            yvalues[[1 for j in 1:(i-1)]...,1,index...] = yval1 +(yval2-yval1)*(xtarget-xval1)/(xval2-xval1)
+    for i in 1:dimensions
+        xval1 = grid_values[i,1]
+        xval2 = grid_values[i,2]
+        xtarget = grid_parameters[i]
+        for index1 in CartesianIndices(ntuple(j->interpolator_index1_element(j,i),dimensions))
+            index2 = CartesianIndex(ntuple(j->interpolator_index2_element(j,i, index1),dimensions))
+            yval1 = yvalues[index1]
+            yval2 = yvalues[index2]
+            yvalues[index1] = yval1 +(yval2-yval1)*(xtarget-xval1)/(xval2-xval1)
         end
     end
 
-    return yvalues[[1 for i in 1:length(grid.input_names)]...]
-
+    return yvalues[1]
 end
