97d1e721569182291ef6b548f4f00a74cb7fc277
Pablo <pamarca@gmail.com>
2025-05-20 21:28:37 +0200

    Added support for simulation sets that are not contained in a grid

---
+++ src/StellarModelSet.jl
@@ -0,0 +1,56 @@
+using Base.Threads, LinearAlgebra
+
+export StellarModelSet, interpolate_grid_quantity
+
+mutable struct StellarModelSet{N,P,LU,E,V}
+    models::Vector{SimulationData}
+    inputs::Matrix{String}
+    input_names::Vector{Symbol}
+    input_values::Matrix{Float64}
+    simplex_interpolant::SimplexInterpolant{N,P,LU,E,V}
+end
+
+function StellarModelSet(inputs, input_names, path_constructor, dataframe_loader, EEP_and_distance_calculator!; input_values = nothing)
+    if isnothing(input_values)
+        input_values = parse.(Float64, inputs)
+    else
+        if size(input_values) != size(inputs)
+            throw(DomainError(input_values, "Length of input_names and input_values must match"))
+        end
+    end
+    models = Vector{SimulationData}(undef,size(inputs)[2])
+    # load up data
+    #@threads for i in 1:(size(inputs)[2])
+    for i in 1:(size(inputs)[2])
+        strings = Vector{String}(undef, length(input_names))
+        for j in eachindex(input_names)
+            strings[j] = inputs[j, i]
+        end
+
+        models[i] = SimulationData(strings, input_names, path_constructor, dataframe_loader, EEP_and_distance_calculator!)
+    end
+    simplex_interpolant = SimplexInterpolant(input_values)
+    return StellarModelSet(models,inputs,input_names, input_values, simplex_interpolant)
+end
+
+function interpolate_grid_quantity(grid::StellarModelSet{N,P,LU,E,V}, grid_parameters, interpolated_quantity, x::T) where{N,P,LU,E,V,T}
+    models = grid.models
+    coords, indeces = interpolation_info(grid_parameters,grid.simplex_interpolant)
+
+    if maximum(coords)==0
+        return NaN
+    end
+
+    # get value of quantity to interpolate at vertices at a given x
+    yvalues::Vector{T} = zeros(T,length(grid.input_names)+1) # if data dimensions are N, interpolating simplex has N+1 points (eg triangle in 2D)
+    for i in eachindex(indeces)
+        model = models[i]
+        try
+            yvalues[i] = get_secondary_EEP(x, model.df, interpolated_quantity)
+        catch
+            return NaN
+        end
+    end
+
+    return dot(coords, yvalues)
+end
