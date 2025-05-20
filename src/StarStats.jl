d8e38a6d62b67b4626eb294b13298b40543bc871
Pablo <pamarca@gmail.com>
2025-05-20 21:28:37 +0200

    Added support for simulation sets that are not contained in a grid

--- src/StarStats.jl
+++ src/StarStats.jl
@@ -3,4 +3,6 @@ module StarStats
 include("EquivalentEvolutionaryPoint.jl")
 include("SimulationData.jl")
 include("StellarModelGrid.jl")
+include("SimplexInterpolation.jl")
+include("StellarModelSet.jl")
 end # module
