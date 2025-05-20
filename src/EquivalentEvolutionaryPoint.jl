e7776100c21cb995acd5e98f97815700cb9a9e31
Pablo <pamarca@gmail.com>
2025-05-20 19:04:59 +0200

    Separate simulation data from stellar model grid

--- src/EquivalentEvolutionaryPoint.jl
+++ src/EquivalentEvolutionaryPoint.jl
@@ -1,11 +1,12 @@
-export get_EEPs, get_secondary_EEP, interpolate_grid_quantity
+export get_EEPs, get_secondary_EEP, compute_distance_and_EEPs!
+
+using DataFrames
 
 function get_EEPs(track, Xc_TAMS)
     return get_EEPs_internal(track.c_h1, track.c_he4, size(track,1), Xc_TAMS)
 end
 
 function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
-    initial_central_h1 = central_h1[1]
     ZAMS_EEP = 0
     IAMS_EEP = 0
     TAMS_EEP = 0
@@ -81,6 +82,63 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
     return EEPs
 end
 
+"""
+   compute_distance_and_EEPs
+
+Function 
+
+# Fields: 
+-   
+"""
+function compute_distance_and_EEPs!(df::DataFrame; Xc_TAMS=1e-10)
+
+    EEPs = get_EEPs(df, Xc_TAMS)
+
+    distance = zeros(size(df,1))
+    delta_log_Teff = 0
+    delta_log_L = 0
+
+    for i in 2:size(df,1)
+        delta_log_Teff = df.logTeff[i]-df.logTeff[i-1]
+        delta_log_L = df.logL[i]-df.logL[i-1]
+
+        distance[i] = distance[i-1] + sqrt(delta_log_Teff^2 + delta_log_L^2)
+    end
+    df[!,:distance] = distance
+
+    x = zeros(size(df,1))
+    for j in 1:(length(EEPs)-1)
+        start_EEP = EEPs[j]
+        end_EEP = EEPs[j+1]
+        if start_EEP==0 || end_EEP==0
+            break
+        end
+        start_distance = df.distance[start_EEP]
+        end_distance = df.distance[end_EEP]
+        for i in start_EEP:end_EEP
+            x[i] = (df.distance[i]-start_distance)/(end_distance-start_distance) + (j-1)
+        end
+    end
+    for i in size(df,1):-1:1
+        if x[i] > 0
+            x[i:end] .= x[i]
+            break
+        end
+    end
+    df[!,:x] = x
+
+    dtdx = zeros(size(df,1))
+    for i in 2:size(df,1)
+        if (df.x[i]-df.x[i-1])<=0
+            continue
+        end
+        dtdx[i] = (df.star_age[i]-df.star_age[i-1])/(df.x[i]-df.x[i-1])
+    end
+    df[!,:dtdx] = dtdx
+
+    return EEPs
+end
+
 function get_secondary_EEP(x::T, track, interpolated_quantity)::T where{T}
     get_secondary_EEP_internal(x, track.x, getproperty(track, interpolated_quantity))
 end
@@ -88,74 +146,4 @@ function get_secondary_EEP_internal(x::T, trackx, interp_values)::T where{T}
     #dirty check to not overflow at edge
     lower_i = min(searchsorted(trackx,x).stop, length(trackx)-1)
     return interp_values[lower_i] + (x-trackx[lower_i])/(trackx[lower_i+1]-trackx[lower_i])*(interp_values[lower_i+1]-interp_values[lower_i]) # interpolate in x
-end
-
-function interpolate_grid_quantity(grid, grid_parameters, interpolated_quantity, x::T) where{T}
-    return interpolate_grid_quantity_internal(grid.dfs, grid.input_values, grid_parameters, interpolated_quantity, x)
-end
-
-function df_index_element(i, lower_i, cartesian_index)
-    return cartesian_index[i]+lower_i[i]-1
-end
-function interpolator_index1_element(j,i)
-    if j<= i
-        return 1
-    end
-    return 2
-end
-function interpolator_index2_element(j,i,index1)
-    if j< i
-        return 1
-    elseif j==i
-        return 2
-    end
-    return index1[j]
-end
-
-function interpolate_grid_quantity_internal(dfs, input_values, grid_parameters, interpolated_quantity, x::T) where{T}
-    dimensions = ndims(dfs)
-    lower_i = zeros(Int, dimensions)
-    grid_values = zeros(dimensions,2)
-    # get values of parameters at edges
-    for j in 1:dimensions
-        for i in 1:(length(input_values[j])-1)
-            if input_values[j][i] <= grid_parameters[j] && input_values[j][i+1] >= grid_parameters[j]
-                grid_values[j,1] = input_values[j][i]
-                grid_values[j,2] = input_values[j][i+1]
-                lower_i[j] = i
-                break
-            end
-        end
-    end
-    for i in lower_i
-        if i == 0
-            return NaN
-        end
-    end
-
-    # get value of quantity to interpolate at vertices at a given x
-    yvalues::Array{T,dimensions} = zeros(T,ntuple(_->2, dimensions)...)
-    for index in CartesianIndices(yvalues)
-        df_index = ntuple(i->df_index_element(i,lower_i,index),dimensions)
-        try
-            df = dfs[CartesianIndex(df_index)]
-            yvalues[index] = get_secondary_EEP(x, df, interpolated_quantity)
-        catch
-            return NaN
-        end
-    end
-
-    for i in 1:dimensions
-        xval1 = grid_values[i,1]
-        xval2 = grid_values[i,2]
-        xtarget = grid_parameters[i]
-        for index1 in CartesianIndices(ntuple(j->interpolator_index1_element(j,i),dimensions))
-            index2 = CartesianIndex(ntuple(j->interpolator_index2_element(j,i, index1),dimensions))
-            yval1 = yvalues[index1]
-            yval2 = yvalues[index2]
-            yvalues[index1] = yval1 +(yval2-yval1)*(xtarget-xval1)/(xval2-xval1)
-        end
-    end
-
-    return yvalues[1]
 end
