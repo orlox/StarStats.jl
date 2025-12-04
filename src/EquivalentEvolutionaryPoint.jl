6c06d13151e6b5323d7f9b5c30b26638590d0547
orlox <pamarca@gmail.com>
2025-12-04 09:07:45 +0100

    Attempting to fix Brott loader

--- src/EquivalentEvolutionaryPoint.jl
+++ src/EquivalentEvolutionaryPoint.jl
@@ -6,17 +6,14 @@ function get_EEPs(track, Xc_TAMS)
     return get_EEPs_internal(track.c_h1, track.c_he4, size(track,1), Xc_TAMS)
 end
 
-function names_of_EEPs_single_star!(df)
-    names = []
-    push!(names,"ZAMS_EEP")
-    push!(names,"IAMS_EEP")
-    push!(names, "TAMS_EEP")
-    push!(names, "RGBTip_EEP")
-    push!(names,"ZACHeB_EEP" )
-    push!(names, "TACHeB_EEP ")
-    #println()
-    names_new = Symbol.(names)
-    return names_new
+function names_of_EEPs_single_star!()
+    names = [:ZAMS,
+             :IAMS,
+             :TAMS,
+             :RGBTip,
+             :ZACHeB,
+             :TACHe]
+    return names
 end
 function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
     ZAMS_EEP = 0
@@ -36,7 +33,7 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
         end
     end
     if ZAMS_EEP == 0
-        return EEPs
+        return EEPs, names_of_EEPs_single_star!()
     end
     EEPs[1] = ZAMS_EEP
     # IAMS_EEP
@@ -47,7 +44,7 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
         end
     end
     if IAMS_EEP == 0
-        return EEPs
+        return EEPs, names_of_EEPs_single_star!()
     end
     EEPs[2] = IAMS_EEP
     # TAMS_EEP
@@ -58,7 +55,7 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
         end
     end
     if TAMS_EEP == 0
-        return EEPs
+        return EEPs, names_of_EEPs_single_star!()
     end
     EEPs[3] = TAMS_EEP
     # RGBTip_EEP
@@ -69,7 +66,7 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
         end
     end
     if RGBTip_EEP == 0
-        return EEPs
+        return EEPs, names_of_EEPs_single_star!()
     end
     EEPs[4] = RGBTip_EEP
     # ZACHeB_EEP
@@ -80,7 +77,7 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
         end
     end
     if ZACHeB_EEP == 0
-        return EEPs
+        return EEPs, names_of_EEPs_single_star!()
     end
     EEPs[5] = ZACHeB_EEP
     # TACHeB_EEP
@@ -91,7 +88,7 @@ function get_EEPs_internal(central_h1, central_he4, nrows, Xc_TAMS)
         end
     end
     EEPs[6] = TACHeB_EEP
-    return EEPs
+    return EEPs, names_of_EEPs_single_star!()
 end
 
 """
@@ -104,7 +101,7 @@ Function
 """
 function compute_distance_and_EEPs!(df::DataFrame; Xc_TAMS=1e-10)
 
-    EEPs = get_EEPs(df, Xc_TAMS)
+    EEPs, EEP_names = get_EEPs(df, Xc_TAMS)
 
     distance = zeros(size(df,1))
     delta_log_Teff = 0
@@ -148,7 +145,7 @@ function compute_distance_and_EEPs!(df::DataFrame; Xc_TAMS=1e-10)
     end
     df[!,:dtdx] = dtdx
 
-    return EEPs
+    return EEPs, EEP_names
 end
 
 function get_secondary_EEP(x::T, track, interpolated_quantity)::T where{T}
